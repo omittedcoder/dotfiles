cf660db6ac40bd3f25af06618e37b2caecaeb495
omittedcoder <22686398+omittedcoder@users.noreply.github.com>
2025-08-21 17:21:49 +0200

    init

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting ''
+
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+	#set -g __fish_git_prompt_showupstream auto
+	set -g __fish_git_prompt_show_informative_status yes
+	set -g __fish_git_prompt_showcolorhints yes
+    printf '%s%s> ' (prompt_pwd) (fish_git_prompt)
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+	set -l last_status $status
+	# Prompt status only if it's not 0
+	set -l stat
+	if test $last_status -ne 0
+  		echo (set_color red)"[$last_status]"(set_color normal)
+	end
+end
