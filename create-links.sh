53206b6fcd851c399e880b49f0df1de7c084c969
omittedcoder <22686398+omittedcoder@users.noreply.github.com>
2025-12-10 16:58:43 +0100

    add script to create links

---
+++ create-links.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s $(pwd)/bash_aliases ~/.bash_aliases
+ln -s $(pwd)/config.fish ~/.config/fish/config.fish
+ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/i3/config_desktop ~/.config/i3/config
+ln -s $(pwd)/i3/i3blocks_desktop ~/.i3blocks.conf
+ln -s $(pwd)/i3/picom.conf ~/.config/picom/picom.conf
