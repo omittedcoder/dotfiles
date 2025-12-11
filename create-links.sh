73540f56cefcced17dcb56ca52afc97338e37b72
omittedcoder <22686398+omittedcoder@users.noreply.github.com>
2025-12-11 20:06:50 +0100

    improve link script

--- create-links.sh
+++ create-links.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-ln -s $(pwd)/bash_aliases ~/.bash_aliases
-ln -s $(pwd)/config.fish ~/.config/fish/config.fish
-ln -s $(pwd)/gitconfig ~/.gitconfig
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/i3/config_desktop ~/.config/i3/config
-ln -s $(pwd)/i3/i3blocks_desktop ~/.i3blocks.conf
-ln -s $(pwd)/i3/picom.conf ~/.config/picom/picom.conf
+LN_PATH=$(pwd)
+LN_ARGS="--backup=t -s"
+
+ln ${LN_ARGS} ${LN_PATH}/bash_aliases ~/.bash_aliases
+mkdir -p ~/.config/fish
+ln ${LN_ARGS} ${LN_PATH}/config.fish ~/.config/fish/config.fish
+ln ${LN_ARGS} ${LN_PATH}/gitconfig ~/.gitconfig
+ln ${LN_ARGS} ${LN_PATH}/vimrc ~/.vimrc
+mkdir -p ~/.config/i3
+ln ${LN_ARGS} ${LN_PATH}/i3/config_desktop.conf ~/.config/i3/config
+ln ${LN_ARGS} ${LN_PATH}/i3/i3blocks_desktop.conf ~/.i3blocks.conf
+mkdir -p ~/.config/picom
+ln ${LN_ARGS} ${LN_PATH}/i3/picom.conf ~/.config/picom/picom.conf
